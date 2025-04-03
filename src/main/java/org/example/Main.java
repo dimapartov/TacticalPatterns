6cae6cdd8fc503e2ddeef9e7ea0ae9eda4a94b28
dimapartov <mr_kubarev@mail.ru>
2025-04-03 11:14:06 +0300

    Hotfix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -33,38 +33,29 @@ public class Main {
         LocalDateTime dateTimeForShift2End = LocalDateTime.of(2025, 3, 25, 22, 0); // Конец второй смены
 
         // Создание дат для проверки пересекающейся смены
-        LocalDateTime dateTimeForShiftTestStart = LocalDateTime.of(2025, 3, 25, 17, 0); // Старт
+        LocalDateTime dateTimeForShiftTestStart = LocalDateTime.of(2025, 3, 25, 16, 0); // Старт
         LocalDateTime dateTimeForShiftTestEnd = LocalDateTime.of(2025, 3, 25, 18, 0); // Конец
 
-
-        // Создание репозитория и регистрация водителя
         DriverRepository repository = new DriverRepository();
         Driver driver = DriverFactory.createDriver("Ivan Petrov", true);
         repository.save(driver);
 
-        // Создание транзакционного скрипта для управления расписанием
         ScheduleTransactionScript transactionScript = new ScheduleTransactionScript(repository);
 
-        // Добавление валидных смен водителя
         transactionScript.addShiftToDriver(driver.getId(), dateTimeForShift1Start, dateTimeForShift1End);
         transactionScript.addShiftToDriver(driver.getId(), dateTimeForShift2Start, dateTimeForShift2End);
 
-        // Демонстрация возникновения ошибки: попытка добавить пересекающуюся смену
         System.out.println("\nПопытка добавить пересекающуюся смену:");
         transactionScript.addShiftToDriver(driver.getId(), dateTimeForShiftTestStart, dateTimeForShiftTestEnd);
 
 /*
-        // Демонстрация возникновения ошибки: попытка добавить смену для несуществующего водителя
-        System.out.println("\nПопытка добавить смену для несуществующего водителя:");
         transactionScript.addShiftToDriver(999, // несуществующий ID
                 dateTimeForShift1Start, dateTimeForShift1End);
 */
 
-        // Получение смен на определённую дату
         List<Shift> shifts = transactionScript.getShiftsForDriverOnDate(driver.getId(), dateTimeForShift1Start);
         System.out.println("\nСмены на " + dateTimeForShift1Start.toLocalDate() + ": " + shifts);
 
-        // Проверка доступности водителя в конкретное время, в данном примере во время 1 смены
         boolean available = transactionScript.checkDriverAvailability(driver.getId(), dateTimeForShift1Start);
         System.out.println("\nДоступность водителя на " + dateTimeForShift1Start + ": " + available);
     }
